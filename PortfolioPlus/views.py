b1bf60ac9035b9e62a1deec99be18180315360a4
Befrimon <poloskovnicita@yandex.ru>
2024-02-05 12:55:53 +0000

    Add 404 page

--- PortfolioPlus/views.py
+++ PortfolioPlus/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "closed.html", {})
+
+
+def handler404(request):
+    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
