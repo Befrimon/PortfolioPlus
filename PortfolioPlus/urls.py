8db5a74bf80da7cb41db2be8a51c607e2b3410c0
Befrimon <poloskovnicita@yandex.ru>
2024-02-28 22:21:47 +0300

    Fix css

--- PortfolioPlus/urls.py
+++ PortfolioPlus/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="closed"),
+    path('1488/', views.pronation, name="1488"),
     path('handbook/', include("handbook.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- PortfolioPlus/views.py
+++ PortfolioPlus/views.py
@@ -5,6 +5,10 @@ def index(request):
     return render(request, "closed.html", {})
 
 
+def pronation(request):
+    return render(request, "pronation.html", {})
+
+
 def handler404(request):
     response = render_to_response('404.html', {}, context_instance=RequestContext(request))
     response.status_code = 404
